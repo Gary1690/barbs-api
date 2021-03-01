92abf23971d6b8e81ec2e9daf0a0f6e6c03ce927
Gary Cordero <GaryCordero1690@gmail.com>
2021-02-28 22:45:32 -0500

    user controller added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def show 
+    
+  end 
+
+  def update
+
+  end 
+
+  private
+  def user_params 
+    params.required(:user).permit(:name,:lastname,:email,:password)
+  end
+
 end
