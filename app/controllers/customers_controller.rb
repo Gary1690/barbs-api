14efdefc8a3be47e2f88ae1c17db6c322c5627e3
Gary Cordero <GaryCordero1690@gmail.com>
2021-03-02 11:53:02 -0500

    customer controller actions completed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,39 @@ class CustomersController < ApplicationController
     customers = Customer.all
     render json: customers
   end 
+
+  def update
+    customer ||= Customer.find(params[:id])
+    if(customer)
+      customer.update(customer_params)
+      render json: customer
+    else
+      render json: {error:"Customer Couldnt be found."}
+    end
+  end
+
+  def create 
+    customer = Customer.new(customer_params)
+    if(customer.save)
+      render json: customer
+    else
+      render json: {error:"Customer Couldnt be created."}
+    end
+  end 
+
+  def destroy
+    customer ||= Customer.find(params[:id])
+    if(customer)
+      customer.destroy
+      render json: customer
+    else
+      render json: {error:"Customer Couldnt be found."}
+    end
+  end 
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name,:lastname,:email,:number)
+  end 
+
 end
